c76da9f464081b69a427f854a7123a4acd55737c
Alistair Vu <alistairvu@gmail.com>
2021-02-24 17:04:25 +0700

    Replace bootstrpa with chakra

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,9 +1,23 @@
+import { Center, Stack, Heading, Link, Code, Text } from "@chakra-ui/react"
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-      <h2>Start editing the App.tsx file to begin work on your app.</h2>
-    </div>
+    <main>
+      <Center h="100vh" w="100vw">
+        <Stack>
+          <Heading size="4xl" as="h1" textAlign="center">
+            Welcome!
+          </Heading>
+
+          <Text textAlign="center" style={{ fontSize: 30 }}>
+            Get started by editing{" "}
+            <Code colorScheme="red" style={{ fontSize: 30 }}>
+              src/App.tsx
+            </Code>
+          </Text>
+        </Stack>
+      </Center>
+    </main>
   )
 }
 
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,6 +1,11 @@
 import { render } from "react-dom"
 import App from "./App"
-import "./bootstrap.min.css"
+import { ChakraProvider } from "@chakra-ui/react"
 
 const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  rootElement
+)
