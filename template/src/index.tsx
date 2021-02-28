cd1d51d340efe1f3a4a719d817c632fecc2d3fdf
Alistair Vu <alistairvu@gmail.com>
2021-02-28 21:20:51 +0700

    Fix index file

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,12 +1,6 @@
 import { render } from "react-dom"
 import App from "./App"
 import "./bootstrap.min.css"
-import { ChakraProvider } from "@chakra-ui/react"
 
 const rootElement = document.getElementById("root")
-render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-  rootElement
-)
+render(<App />, rootElement)
