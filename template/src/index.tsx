6bb814b9726811cb5df89acc0b74c81e10c5595f
Alistair Vu <alistairvu@gmail.com>
2021-03-17 10:48:25 +0700

    Add scss

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from "react-dom"
 import App from "./App"
-import "./bootstrap.min.css"
+import "./styles/index.scss"
 
 const rootElement = document.getElementById("root")
 render(<App />, rootElement)
